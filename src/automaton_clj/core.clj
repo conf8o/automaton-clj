5ffd4b59709f2110f01f887881e3fbfb015ba29a
conf8o <41929738+conf8o@users.noreply.github.com>
2021-06-30 15:57:21 +0900

    Alogorithmic automaton

--- src/automaton_clj/core.clj
+++ src/automaton_clj/core.clj
@@ -1,17 +1,50 @@
-(ns automaton-clj.core)
+(ns automaton-clj.core
+  (:require [clojure.string :as string]))
 
+; 関数としての決定性有限オートマトン
 (def characters #{\a \b})
 (defn head [[c & _]] (characters c))
-(defmulti one head)
-(defmulti two head)
-(defmulti three head)
+(defmulti q1 head)
+(defmulti q2 head)
+(defmulti q3 head)
 
-(defmethod one \a [[_ & tail]] (two tail))
-(defmethod one \b [[_ & tail]] (one tail))
-(defmethod one :default [_] (println "Rejected on one"))
+(defmethod q1 \a [[_ & tail]] (q2 tail))
+(defmethod q1 \b [[_ & tail]] (q1 tail))
+(defmethod q1 :default [_] (println "Rejected on q1"))
 
-(defmethod two \a [[_ & tail]] (two tail))
-(defmethod two \b [[_ & tail]] (three tail))
-(defmethod two :default [_] (println "Rejected on two"))
+(defmethod q2 \a [[_ & tail]] (q2 tail))
+(defmethod q2 \b [[_ & tail]] (q3 tail))
+(defmethod q2 :default [_] (println "Rejected on q2"))
 
-(defmethod three :default [_] (do (println "Accepted") :accepted))
+(defmethod q3 \a [[_ & tail]] (q3 tail))
+(defmethod q3 \b [[_ & tail]] (q3 tail))
+(defmethod q3 :default [_] (do (println "Accepted") :accepted))
+
+; アルゴリズム的な決定性有限オートマトン
+(defn dfa [charcters mapping acceptions]
+  (let [index (zipmap charcters (range))]
+    (fn [state [head & tail]]
+      (loop [s state
+             h head
+             t tail]
+        (cond
+          (and (mapping s) (index h))
+          (recur ((mapping s) (index h))
+                 (first t)
+                 (rest t))
+
+          (and (nil? h) (acceptions s))
+          {:accepted s}
+
+          :else
+          {:rejected s
+           :rest (string/join (list* h t))})))))
+
+
+(def automaton
+  (dfa
+   [\a \b]
+   {:q1 [:q2 :q1]
+    :q2 [:q2 :q3]
+    :q3 [:q3 :q3]}
+   #{:q3}))
